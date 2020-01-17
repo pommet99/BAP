3ead8f6dcae3be9600b01c30ccf8d7498ba48094
Prameet <47789905+pommet99@users.noreply.github.com>
2020-01-17 13:04:28 +0100

    Added base Desktop website

---
+++ CustomTheme/content.php
@@ -0,0 +1,83 @@
+
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="<?php echo get_template_directory_uri(); ?>/photos/strech.jpg" class="d-block w-100">
+        </div>
+        <div class="carousel-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/photos/laught.jpg" class="d-block w-100">
+        </div> 
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+
+<!-- Body  -->
+
+<div class="container2">
+<div class="page1">
+    <div class="row">
+        <div class="col-12">
+            <h2 id="titre1">QUI <br> SOMMES <br> NOUS ?</h2>
+            <a class="p1">FamoSport est une association qui a pour objectif de développer le sport féminin dans les QPV et par la même occasion de dynamiser les quartiers par le biais de la pratique sportive.
+            <br><br><br>
+            Nous souhaitons répondre aux problématiques que rencontrent les femmes lorsqu'elles souhaient pratiquer une activité sportive (manque de temps, de connaissances, d'estime de soi, contraintes financières, familiales ...).
+            <br><br><br>
+            Nous avons donc mis en place des services et des activités sportives adaptés afin qu'elles puissent se remettre en sport et devenir indépendante dans leur pratique.
+            </a>
+        </div>
+    </div>
+</div>
+
+<div class="espace"></div>
+
+  <div class="page2">
+    <div class="row">
+        <div class="col-12">
+            <h2 id="titre2">EVENEMENTS</h2>
+            
+
+            
+        </div>
+    </div>
+</div>
+
+<div class="espace"></div>
+
+  <div class="page3">
+    <div class="row">
+        <div class="col-12">
+            <h2 id="titre3">ACTIVITES</h2>
+            <br><br>
+            <a class="p2">Besoin de souffler, de prendre du temps pour soi ?
+            <br><br>
+            FamoSport est là pour vous apporter la solution.Nous vous proposons des séances de sport encadrées par des coachs professionnels qui vous accompagnerons tout au long pour vous garantir une session dans les meilleures conditions.<br><br>
+            Si votre enfant souhaite se joindre à vous, nous vous permettons de garder vos enfants en bas âge par du personnel qualifié ce qui vous permettra d'effectuer votre séance sereinement et de récupérer votre enfant ensuite.
+            Pour finir sur une note réconfortante, chaque séance est ponctuée d'un buffet healthy disponible pour toutes.
+            Nous vous promettons que nos activités permettront de renforcer les liens avec vos enfants.
+            </a>
+            <br>
+        </div>
+    </div>
+</div>
+
+    <div class="background">
+    <div class="page4">
+     <div class="row">
+      <div class="col-12">
+      <iframe id="haWidget2" allowtransparency="true" src="https://www.helloasso.com/associations/famosport/formulaires/1/widget-bouton"></iframe><div style="width:100%;text-align:center;"></div>
+      </div>   
+     </div>
+    </div>
+   </div>
+
+</div>
+
+</div>
---
+++ CustomTheme/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Oxygen&display=swap" rel="stylesheet">
+ 
+    <?php wp_head(); ?> 
+</head>
+
+<body>
+<header>
+   <div class="container1">
+     <div class="row">
+
+       <div class="col-sm-3">
+       <img src="<?php echo get_template_directory_uri(); ?>/photos/logo.png" alt="logo" class="logo-nav">
+       </div>
+    
+       <div class="col-sm-6">
+       <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link" href="#titre1">QUI SOMMES NOUS ?</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#titre2">EVENEMENTS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#titre3">ACTIVITES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://docs.google.com/forms/d/e/1FAIpQLSfR8WHmL8zZB0EK1hD5yhlaU19R49t3UCuDy-EvayPxUZMTNA/viewform?fbclid=IwAR2jxPE-N8Wh8BzTpTX7NMwmvBaEve4adMjLbevsiz4jYkLWz9JAT2BOwyA">S'INSCRIRE</a>
+            </li>
+        </ul>
+        </div>
+
+       <div class= col-sm-3>
+       <iframe id="haWidget" allowtransparency="true" src="https://www.helloasso.com/associations/famosport/formulaires/1/widget-bouton" style="width:100%;height:70px;border:none;"></iframe><div style="width:100%;text-align:center;"></div>
+       </div>
+</div>
+</div> 
+     
+</header>
